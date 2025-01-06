dd2adcdb5836316f7b2cd9ad0c68a9380a5d7f4e
arronite <ary45hira2i@gmail.com>
2025-01-06 13:38:37 +0330

    minor changes

--- queries/Comments.sql
+++ queries/Comments.sql
@@ -1,8 +1,7 @@
 CREATE TABLE Comments (
     rand_id VARCHAR(255),
     body text,
-    parent_id INTEGER,
-    publish_status BOOLEAN,
+    publish_status Publish_status,
     video_id VARCHAR(255), 
     cr_rand_id VARCHAR(255),
     cr TIMESTAMP WITH TIME ZONE,
